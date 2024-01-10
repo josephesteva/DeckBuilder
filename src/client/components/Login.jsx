243e9d727449a8296e705991e1e1f6c52b7a82bb
Gregory Jean Baptiste <gregster0@gmail.com>
2024-01-10 15:47:43 -0500

    navbar + login and register routes skeleton

---
+++ src/client/components/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <>
+            This is the Login Page
+        </>
+    )
+}
+
+export default Login;
---
+++ src/client/components/NavBar.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <button>This</button>
+            <button>is</button>
+            <button>the</button>
+            <button>NavBar</button>
+        </nav>
+    )
+}
+
+export default NavBar;
