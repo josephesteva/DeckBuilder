35ce464550f1313f35e6d784fc0f0149bc1f22bc
josephesteva <joe.esteva@gmail.com>
2024-01-10 14:56:21 -0500

    Implement initial auth register endpoint

--- src/server/auth/index.js
+++ src/server/auth/index.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+
 
 router.get('/test', (req, res, next) => {
 	res.send('Test Auth Endpoint')
+})
+
+router.post('/register', async (req, res, next) => {
+	const {username, email, password} = req.body;
+	const SALT_ROUNDS = 5;
+	const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+	try {
+		const newUser = await prisma.user.create({
+			data: {
+				username,
+				email,
+				password: hashedPassword,
+				isAdmin: false
+			}
+		})
+		res.status(201).send(newUser)
+	} catch (err) {
+		console.error(err);
+	}
 })
 
 module.exports = router;
