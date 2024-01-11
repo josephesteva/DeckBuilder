da097c3e06aa9f305831ae441309b0aa414ae495
josephesteva <joe.esteva@gmail.com>
2024-01-11 12:04:29 -0500

    Add deck get endpoints

--- src/server/api/decks.js
+++ src/server/api/decks.js
@@ -1,7 +1,33 @@
+const { PrismaClient } = require('.prisma/client');
+const prisma = new PrismaClient()
+
 const router = require('express').Router();
 
 router.get('/test', (req, res, next) => {
 	res.send("Test Decks Endpoint")
+})
+
+router.get('/', async (req, res, next) => {
+	try {
+		const decks = await prisma.deck.findMany({});
+		res.status(200).send(decks)
+	} catch (err) {
+		console.error(err);
+	}
+})
+
+router.get('/:id', async (req, res, next) => {
+	const {id} = req.params
+	try {
+		const deck = await prisma.deck.findUnique({
+			where: {
+				id: +id,
+			},
+		})
+		res.status(200).send(deck)
+	} catch (err) {
+		console.error(err);
+	}
 })
 
 module.exports = router;
