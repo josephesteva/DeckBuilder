210c52f51040f975a31b7970d9d6f1180cef86d6
josephesteva <joe.esteva@gmail.com>
2024-01-11 15:21:36 -0500

    Implement initial get endpoints for cardsondecks

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/users', require('./users.js'));
 router.use('/decks', require('./decks.js'));
 router.use('/cards', require('./cards.js'));
+router.use('/deckcards', require('./cardsOnDecks.js'));
 
 module.exports = router;
