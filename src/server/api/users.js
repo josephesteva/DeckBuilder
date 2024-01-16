4f8c9fd829921813dbce0a62706a8a4aa3ad2810
josephesteva <joe.esteva@gmail.com>
2024-01-16 11:31:01 -0500

    Update users.js

--- src/server/api/users.js
+++ src/server/api/users.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('.prisma/client');
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
 const router = require('express').Router();
