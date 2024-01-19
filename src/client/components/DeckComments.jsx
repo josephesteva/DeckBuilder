f040bb57231245a6055c1b14cbbd89cdee146a9d
josephesteva <joe.esteva@gmail.com>
2024-01-19 11:14:22 -0500

    Implement initial rendering of comments on a deck

--- src/client/components/DeckComments.jsx
+++ src/client/components/DeckComments.jsx
@@ -3,8 +3,9 @@ import "../App.css";
 import React, { useEffect, useState } from 'react';
 
 const DeckComments = () => {
-	const [ deckId, setDeckId ] = useState("")
-	const [ content, setContent ] = useState("")
+	const [deckId, setDeckId] = useState("")
+	const [commentThread, setCommentThread] = useState([])
+	const [content, setContent] = useState("")
 
 	const handleCreateComment = async () => {
 		try {
@@ -15,23 +16,44 @@ const DeckComments = () => {
 						Authorization: "Bearer " + window.localStorage.getItem("token"),
 					}
 				});
-				console.log(comment);
+			console.log(comment);
 		} catch (err) {
 			console.error(err);
 		}
 	}
 
+	const handleShowComments = async () => {
+		try {
+			const {data} = await axios.get(`/api/comments/ondeck/${deckId}`)
+			setCommentThread(data)
+			console.log(data)
+		} catch (err) {
+			console.error(err);
+		}
+		
+	}
+
 	return (
 		<>
 			<div>
-				<h3>Comments</h3>
-				<hr />
+				<h4>Deck ID: </h4>
 				<input
 					placeholder="Deck ID here"
-					value = {deckId}
+					value={deckId}
 					onChange={(e) => setDeckId(e.target.value)}
 				/>
-				<hr/>
+				<hr />
+				<h3>Comments</h3>
+				<button onClick={handleShowComments}>Show Comments</button>
+				<div>
+					{commentThread.map((comment) => (
+						<div style={{border: "solid black .1em", margin: ".5em"}} key={comment.id} >
+							<p> {comment.content}</p>
+							<p> Posted by {comment.user.username} at {comment.date}</p>
+						</div>
+					))}
+				</div>
+				<hr />
 				<textarea
 					placeholder="Input comment here..."
 					value={content}
