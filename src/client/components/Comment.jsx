f65b5d3dfc7317e690fcfcb2dd23e60b11f18992
josephesteva <joe.esteva@gmail.com>
2024-02-02 10:12:20 -0500

    Add deletion functionality for comments

--- src/client/components/Comment.jsx
+++ src/client/components/Comment.jsx
@@ -5,10 +5,21 @@ function Comment({ comment, userId }) {
 	const [tempComment, setTempComment] = useState(comment.content)
 	const [editing, setEditing] = useState(false)
 	const [date, setDate] = useState("")
+	const [deleted, setDeleted] = useState(false)
 
 	const handleEditClick = () => {
 		setTempComment(comment.content)
 		setEditing(!editing)
+	}
+// TODO: Add some form of confirmation for deletion
+	const handleDeleteClick = async () => {
+		try {
+			const {data: deletedComment } = await axios.delete(`/api/comments/${comment.id}`)
+			console.log(deletedComment);
+			setDeleted(true);
+		} catch (error) {
+			console.error(error);
+		}
 	}
 
 	useEffect(() => {
@@ -17,23 +28,26 @@ function Comment({ comment, userId }) {
 	}, [])
 
 	const handleUpdateComment = async () => {
-		console.log(tempComment);
-		console.log(comment.id);
-		const { data: updatedComment } = await axios.patch(`/api/comments/${comment.id}`,
-		{
-			content: tempComment
-		},
-		{
-			headers: {
-				Authorization: "Bearer " + localStorage.getItem('token')
-			}
-		})
+		try {
+			const { data: updatedComment } = await axios.patch(`/api/comments/${comment.id}`,
+			{
+				content: tempComment
+			},
+			{
+				headers: {
+					Authorization: "Bearer " + localStorage.getItem('token')
+				}
+			})
+		} catch (error) {
+			console.error(error);
+		}
 		comment.content = tempComment
 		setEditing(!editing)
 		console.log(updatedComment);
 	}
 
 	return (
+		<> {!deleted ? (
 		<>
 			<div style={{ border: "solid black .1em", margin: ".5em", borderRadius: ".5em"  }}>
 				{
@@ -54,12 +68,17 @@ function Comment({ comment, userId }) {
 				}
 				<p> Posted by {comment.user.username} on {date.slice(0, 24)}</p>
 				{comment.userId == localStorage.getItem('userId') && !editing ?
-					(
+					(<>
 						<button onClick={handleEditClick}>Edit Comment</button>
+						<button onClick={handleDeleteClick}>Delete Comment</button>
+					</>
 					) : (
 						<></>
 					)}
 			</div>
+		</>) : (
+			null
+		)}
 		</>
 	)
 }
