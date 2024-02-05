bced42ea47fd2ede7926ede6344feee5b0b38c38
Joshua Dogma <jdogma94@gmail.com>
2024-02-05 15:41:33 -0500

    Add watch page to explore. Add minor css changes

---
+++ src/client/components/ExploreWatchBattle.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+const WatchBattle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            // Stop observing once the section is visible to avoid unnecessary checks
+            observer.unobserve(sectionRef.current);
+          }
+        });
+      },
+      { threshold: 1 } // Adjust the threshold / Doesn't impact need . 
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    // Clean up the observer when the component unmounts
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  
+  return (
+    <div ref={sectionRef} className={`watch-live-battle-section ${isVisible ? 'visible' : ''}`}>
+    {isVisible && (
+      <>
+      <div className='prepare'>
+      <h2>Coming Soon</h2>
+      <p>Exciting News!</p>
+      <p>Discover a new feature that allows you to seamlessly convert your custom Pokémon deck into an importable format. This groundbreaking functionality enables you to share your strategic decks with friends or quickly load them for battle.</p>
+      <p>Stay tuned for a step-by-step guide on how to utilize this feature and take your Pokémon battles to the next level!</p>
+      </div>
+      <h4>Watch our Trainers compete in Live Battle</h4>
+      <iframe
+          title="Live Pokemon Battle"
+          width="1400"
+          height="400"
+          src="https://replay.pokemonshowdown.com/oumonotype-82345404"
+          frameBorder="0"
+      ></iframe>
+
+      <img src='/images/Mewtwo.png' alt='MewTwo' className='mewtwo'></img>
+
+      </>
+
+    )}
+    </div>
+  )
+}
+
+export default WatchBattle;
