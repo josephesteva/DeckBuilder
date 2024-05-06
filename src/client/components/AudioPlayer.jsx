27fa9260362d80733966324294e01412d480d0a1
josephesteva <joe.esteva@gmail.com>
2024-05-05 21:51:28 -0400

    Refactor audio component

--- src/client/components/AudioPlayer.jsx
+++ src/client/components/AudioPlayer.jsx
@@ -1,37 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { MdMusicNote } from "react-icons/md";
 import { MdMusicOff } from "react-icons/md";
-
+import PokemonThemeGuitar from "/music/PokémonThemeGuitar.mp3";
 
 const AudioPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio, setAudio] = useState(new Audio(PokemonThemeGuitar));
 
-  const [isPaused, setIsPaused] = useState(false);
+  // useEffect(() => {
+  //   setAudio((audio.loop = true));
+  // }, []);
 
+  console.log(audio);
 
-  const handlePause = () => {
-    setIsPaused(!isPaused);
+  const toggleAudio = () => {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
   };
 
-
-
-
   return (
-
     <div>
-      {!isPaused ? null : (
-      <iframe
-        width="0"
-        height="0"
-        src="https://www.youtube.com/embed/zxKeIAZT7Mw?si=00nqQLgYVcREUtox&amp;disablekb=1&amp;controls=1&amp;autoplay=1&amp;mute=0"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;">
-      </iframe>
-      )}
-
-<button className='audio-container' onClick={handlePause}>{isPaused ? <MdMusicOff className='audio-player' /> :  <MdMusicNote className='audio-player' /> }</button>
+      <button className="audio-container" onClick={toggleAudio}>
+        {isPlaying ? (
+          <div>
+            <MdMusicNote className="audio-player" />
+            Pause
+          </div>
+        ) : (
+          <div>
+            <MdMusicOff className="audio-player" />
+            Play
+          </div>
+        )}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default AudioPlayer;
