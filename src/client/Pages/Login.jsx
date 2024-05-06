9d858c5bfd4c1d0449bd40fd3daa665b16aa1c02
josephesteva <joe.esteva@gmail.com>
2024-05-06 12:08:56 -0400

    Fix login and register font color

--- src/client/Pages/Login.jsx
+++ src/client/Pages/Login.jsx
@@ -1,88 +1,97 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import "../styles/Login.css";
 
-
 const Login = () => {
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-  const [error, setError] = useState('');
-  const navigate = useNavigate()
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
     const userData = {
       username,
-      password
-    }
-    
+      password,
+    };
+
     try {
-      const response = await fetch('/auth/login', {
-        method: 'POST',
+      const response = await fetch("/auth/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(userData)
+        body: JSON.stringify(userData),
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log('Login successful:', data);
-        localStorage.setItem('token', data.token);
+        console.log("Login successful:", data);
+        localStorage.setItem("token", data.token);
 
         if (data.token) {
           const decodedToken = jwtDecode(data.token);
-          localStorage.setItem('userId', decodedToken.id);
-          localStorage.setItem('userName', decodedToken.username);
-          localStorage.setItem('isAdmin', decodedToken.isAdmin);
+          localStorage.setItem("userId", decodedToken.id);
+          localStorage.setItem("userName", decodedToken.username);
+          localStorage.setItem("isAdmin", decodedToken.isAdmin);
         }
         navigate("/");
-
       } else {
         const errorData = await response.json();
-        console.error('Login failed:', errorData.message);
+        console.error("Login failed:", errorData.message);
         // Set error message if request fails
         setError(errorData.message);
       }
     } catch (error) {
-      console.error('Error during Login:', error);
+      console.error("Error during Login:", error);
       // Set error message if request fails
-      setError('An error occurred during login');
+      setError("An error occurred during login");
     }
-  }
+  };
 
   return (
     <div>
-    <div className='background-color'>
-    </div>
-    <div className='form-container'>
-      <h1>Welcome back!</h1>
-      <form onSubmit={handleLogin}>
-        {error && <div className="error-message">{error}</div>}
-        <div>
-          <label></label>
-          <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} required />
-        </div>
-        <div>
-          <label></label>
-          <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </div>
-        <button type="submit">Login</button>
-        <button type="button">Cancel</button>
-      </form>
+      <div className="background-color"></div>
+      <div className="form-container">
+        <h1>Welcome back!</h1>
+        <form onSubmit={handleLogin}>
+          {error && <div className="error-message">{error}</div>}
+          <div>
+            <label></label>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label></label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+          <button type="button">Cancel</button>
+        </form>
 
-      <div class="drops">
-    <div class="drop drop-1"></div>
-    <div class="drop drop-2"></div>
-    <div class="drop drop-3"></div>
-    <div class="drop drop-4"></div>
-    <div class="drop drop-5"></div>
+        <div class="drops">
+          <div class="drop drop-1"></div>
+          <div class="drop drop-2"></div>
+          <div class="drop drop-3"></div>
+          <div class="drop drop-4"></div>
+          <div class="drop drop-5"></div>
+        </div>
+        <img src="/images/login-pokemon.png" className="register-pokemon"></img>
+      </div>
     </div>
-      <img src='/images/login-pokemon.png' className='register-pokemon'></img>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Login;
