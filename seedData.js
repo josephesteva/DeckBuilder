74199f9ac5e553db35b3d535df6b5c892be7c8f8
josephesteva <joe.esteva@gmail.com>
2024-06-27 12:11:33 -0400

    Update seedData

--- seedData.js
+++ seedData.js
@@ -12,16 +12,16 @@ const seedData = {
 		"Ironclad Legion"
 	],
 	deckDescriptions: [
-		"Unleashes a torrent of powerful electric and water-type attacks, overwhelming opponents with its relentless energy.",
-		"Shrouded in mystery, this deck employs illusions and mind-bending tactics to confuse and outmaneuver opponents.",
-		"Fueled by flames, this deck scorches the battlefield with its fiery Pokemon and relentless aggression.",
-		"Harnesses the power of nature, deploying an army of grass and bug-type Pokemon to dominate the battlefield.",
-		"Cloaked in darkness, this deck strikes from the shadows with ghost-type Pokemon and cunning strategies.",
-		"Rules the waves with its water-type Pokemon, creating a tidal wave of unstoppable force.",
-		"Protects the galaxy with an array of cosmic Pokemon, defending against any threat with celestial power.",
-		"Channels the energy of the stars, using psychic and fairy-type Pokemon to control the battlefield with ethereal grace.",
-		"Freezes opponents in their tracks with its ice-type Pokemon, delivering a chilling onslaught of icy attacks.",
-		"Forms an unbreakable line of defense with its steel-type Pokemon, standing firm against any opposition with unwavering resolve."
+		"This deck unleashes a torrent of powerful electric and water-type attacks, overwhelming opponents with its relentless energy.",
+		"This playstyle relies on a shroud of mystery, this deck employs illusions and mind-bending tactics to confuse and outmaneuver opponents.",
+		"This deck is fueled by flames, this deck scorches the battlefield with its fiery Pokemon and relentless aggression.",
+		"These cards harness the power of nature, deploying an army of grass and bug-type Pokemon to dominate the battlefield.",
+		"These cards are cloaked in darkness, this deck strikes from the shadows with ghost-type Pokemon and cunning strategies.",
+		"This playstyle rules the waves with its water-type Pokemon, creating a tidal wave of unstoppable force.",
+		"This deck protects the trainer with an array of cosmic Pokemon, defending against any threat with celestial power.",
+		"These cards channel the energy of the stars, using psychic and fairy-type Pokemon to control the battlefield with ethereal grace.",
+		"This deck freezes opponents in their tracks with its ice-type Pokemon, delivering a chilling onslaught of icy attacks.",
+		"This playstyle forms an unbreakable line of defense with its steel-type Pokemon, standing firm against any opposition with unwavering resolve."
 	],
 	deckComments: [
 		"This deck blitzed opponents with lightning-fast combos, keeping them on their toes with its relentless energy.",
@@ -30,10 +30,10 @@ const seedData = {
 		"This deck burned bright with aggression, scorching opponents with its relentless fiery onslaught.",
 		"Nurturing its Pokemon to overwhelming strength, this deck blossomed with life and vitality.",
 		"An otherworldly aura surrounded this deck, leaving opponents mystified and entranced.",
-		"Standing firm against any challenge, this deck showcased unyielding resilience and determination.",
+		"Standing firm against any challenge, this deck will showcase unyielding resilience and determination.",
 		"Echoing with the might of thunderstorms, this deck shook opponents to their core with its powerful strikes.",
-		"Chilling opponents to the bone, this deck froze their progress with icy control and precision.",
-		"Emanating a sense of peace and tranquility, this deck soothed battles with its harmonious strategy and graceful gameplay."
+		"These cards chill opponents to the bone, freezing their progress with icy control and precision.",
+		"This deck emanates a sense of peace and tranquility, with its harmonious strategy and graceful gameplay."
 	],
 }
 
